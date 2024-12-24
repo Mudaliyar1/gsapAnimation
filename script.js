20c80fe07f51bb0336e300c8499a802ad92efafc
Vijay <vijaymudaliyar224@gmail.com>
2024-12-23 23:22:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -38,23 +38,52 @@
 
 
 
+let startX, endX;
+
+// For desktop
 window.addEventListener("wheel", function (dets) {
     if (dets.deltaY > 0) {
-
         gsap.to("#marque", {
             transform: "translateX(-200%)",
             duration: 1.5,
             repeat: -1,
             ease: "none"
-        })
-
+        });
     } else {
         gsap.to("#marque", {
             transform: "translateX(0%)",
             duration: 1.5,
             repeat: -1,
             ease: "none"
-        })
-        
+        });
     }
-})
+});
+
+// For mobile
+window.addEventListener("touchstart", function (e) {
+    startX = e.touches[0].clientX;
+});
+
+window.addEventListener("touchmove", function (e) {
+    endX = e.touches[0].clientX;
+});
+
+window.addEventListener("touchend", function () {
+    if (startX > endX) {
+        // Swipe left
+        gsap.to("#marque", {
+            transform: "translateX(-200%)",
+            duration: 1.5,
+            repeat: -1,
+            ease: "none"
+        });
+    } else if (startX < endX) {
+        // Swipe right
+        gsap.to("#marque", {
+            transform: "translateX(0%)",
+            duration: 1.5,
+            repeat: -1,
+            ease: "none"
+        });
+    }
+});
